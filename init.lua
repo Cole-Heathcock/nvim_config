c257d118cea2d2f071e58963a7074a1d566271a3
Cole Heathcock <cole.t.heathcock@gmail.com>
2025-03-05 20:49:10 -0500

    Added neogit to config

--- init.lua
+++ init.lua
@@ -300,6 +300,18 @@ require("lazy").setup({
             )
         end
     },
+    {
+      "NeogitOrg/neogit",
+      dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        -- Only one of these is needed.
+        "nvim-telescope/telescope.nvim", -- optional
+      },
+      config = true
+    }
+
 },
 {
     ui = {
